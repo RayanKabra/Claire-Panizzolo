92aaebcb53dd5f7dc9c15d747171993a8c285373
RayanKabra <rkabra.dev@gmail.com>
2024-07-06 12:47:50 +0200

    New backgrounds for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,87 @@
 import React, { useState, useEffect, useMemo } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
+
+// Import Desktop backgrounds
 import background1 from "../assets/images/Home/home1.webp";
 import background2 from "../assets/images//Home/home.jpg";
 import background3 from "../assets/images/Home/home3.webp";
 import background4 from "../assets/images/Home/home4.webp";
 import background5 from "../assets/images/Home/home5.webp";
 
+// Import Mobile backgrounds
+import backgroundMobile1 from "../assets/images/Home/homeMobile1.webp";
+import backgroundMobile2 from "../assets/images/Home/homeMobile2.webp";
+import backgroundMobile3 from "../assets/images/Home/homeMobile3.webp";
+import backgroundMobile4 from "../assets/images/Home/homeMobile4.webp";
+import backgroundMobile5 from "../assets/images/Home/homeMobile5.webp";
+import backgroundMobile6 from "../assets/images/Home/homeMobile6.webp";
+
 const Header = () => {
   const [currentBackground, setCurrentBackground] = useState(0);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [location]);
+
+  // Desktop backgrounds
   const backgrounds = useMemo(
     () => [background1, background2, background3, background4, background5],
     []
   );
 
+  // Mobile backgrounds
+  const backgroundsMobile = useMemo(
+    () => [
+      backgroundMobile1,
+      backgroundMobile2,
+      backgroundMobile3,
+      backgroundMobile4,
+      backgroundMobile5,
+      backgroundMobile6,
+    ],
+    []
+  );
+
   useEffect(() => {
-    backgrounds.forEach((image) => {
+    const imagesToPreload = isMobile ? backgroundsMobile : backgrounds;
+    imagesToPreload.forEach((image) => {
       const img = new Image();
       img.src = image;
     });
-  }, [backgrounds]);
 
-  useEffect(() => {
     const interval = setInterval(() => {
       setCurrentBackground((prevBackground) =>
-        prevBackground === backgrounds.length - 1 ? 0 : prevBackground + 1
+        prevBackground === imagesToPreload.length - 1 ? 0 : prevBackground + 1
       );
-    }, 4000); // Changez 5000 pour définir la durée entre les changements d'images (en millisecondes)
+    }, 4000); // Changez 4000 pour définir la durée entre les changements d'images (en millisecondes)
 
     return () => clearInterval(interval);
-  }, [backgrounds.length]);
+  }, [isMobile, backgrounds, backgroundsMobile]);
 
-  return (
+  return isMobile ? (
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${backgroundsMobile[currentBackground]})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        transition: "background-image 1s ease-in-out",
+      }}
+    >
+      <Navbar scrollControlValue={700} />
+    </div>
+  ) : (
     <div
       className="header"
       style={{
